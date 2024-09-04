d92b5feddb16c93cec4833832bb422ff377ec4eb
Ben Sabath <bsabath1@gmail.com>
2024-09-04 16:06:58 -0400

    test cli run

--- tests/cli/test_cli_run.py
+++ tests/cli/test_cli_run.py
@@ -1,4 +1,6 @@
-from tatm.cli.run import parse_config_opts
+from click.testing import CliRunner
+
+from tatm.cli.run import parse_config_opts, run
 
 
 def test_parse_config_opts():
@@ -6,3 +8,29 @@ def test_parse_config_opts():
     files, overrides = parse_config_opts(opts, validate=False)
     assert files == ["config1", "config2"]
     assert overrides == ["field.subfield=value"]
+
+
+def test_run_tokenize(tmp_path):
+    runner = CliRunner()
+    expected_return = "/usr/bin/sbatch --nodes 1 --cpus-per-task 40 --mem 40G --time 1-00:00:00 --partition debug --account kempner_dev --job-name tatm_tokenize --output tatm_tokenize.out run.submit"
+    with runner.isolated_filesystem(temp_dir=tmp_path):
+        args = [
+            "-N",
+            "1",
+            "-c",
+            "40",
+            "--conf",
+            "slurm.partition=debug",
+            "--conf",
+            "slurm.account=kempner_dev",
+            "--submit-script",
+            "run.submit",
+            "--no-submit",
+            "tokenize",
+            "test_data",
+        ]
+        result = runner.invoke(run, args)
+        print(result.output)
+        assert result.exit_code == 0
+        output = result.output.strip()
+        assert output == expected_return
