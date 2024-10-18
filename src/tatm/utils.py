0eb19adf2f332a9883ad1975142b14aecf5d8a8d
Ben Sabath <bsabath1@gmail.com>
2024-10-18 11:59:18 -0400

    convert (str,Enums) to TatmOptionEnum

--- src/tatm/utils.py
+++ src/tatm/utils.py
@@ -1,3 +1,4 @@
+from enum import Enum
 import logging
 
 
@@ -6,3 +7,14 @@ def configure_logging(log_level=logging.INFO):
         level=log_level,
         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     )
+
+class TatmOptionEnum(str, Enum):
+    """base class for creating a set of Enum options for the Tatm package.
+       
+    Adds has_value method to Enum class to check if a value is in the Enum.
+    
+    """
+
+    @classmethod
+    def has_value(cls, value):
+        return any(value == item.value for item in cls)
---
+++ tests/utils/test_utils.py
@@ -0,0 +1,10 @@
+from tatm.utils import TatmOptionEnum
+
+def test_tatm_option_enum():
+    class TestEnum(TatmOptionEnum):
+        TEST1 = "test1"
+        TEST2 = "test2"
+    
+    assert TestEnum.has_value("test1")
+    assert TestEnum.has_value("test2")
+    assert not TestEnum.has_value("test3")
